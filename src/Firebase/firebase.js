fd874bed38e923e59c9159511606cbbf5ecfb9ac
unknown <omer.marquezt@gmail.com>
2020-11-11 23:06:05 -0400

    Old Firebase Configuration

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 // Keys saved in a .env file, not tracked by Git, create yours to fill it with your own data
 const config = {
@@ -38,9 +39,50 @@ class Firebase {
       }
     }
 
-    this.name = app.app().name
+    this.auth = app.auth();
+    this.name = app.app().name;
   }
+
+  /*** AUTH API INTERFACE ***/
+
+  createUserWithEmailAndPassword  = (email, password) =>{
+    this.auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  signInWithEmailAndPassword = (email, password) =>{
+    this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  signOut = () => this.auth.signOut();
+
+  passwordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  passwordUpdate = password => this.auth.currentUser.updatePassword(password);
 }
 
 // Expose the Firebase class to be used along the App
 export default Firebase;
+
+/**
+ * @todo
+ * Configura un observador de estado de autenticación y obtén datos del usuario (https://firebase.google.com/docs/auth/web/start#set_an_authentication_state_observer_and_get_user_data)
+ * 
+ * firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+  }
+});
+
+  * Maybe use this to update app and know if the user is loged in, or logged out.
+ */
